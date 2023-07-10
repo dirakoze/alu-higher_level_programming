29d98e2d3344cae52c3e1bdb910e02e6be100e4e
dirakoze <d.irakoze@alustudent.com>
2023-07-10 05:25:59 -0700

    tasks

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 def uppercase(str):
-    new_string = ""
     for char in str:
-        if (ord(char) in range(97, 123)):
-            new_string += chr(ord(char) - 32)
-        else:
-            new_string += char
-    print("{new_string}".format(new_string=new_string))
+        if 123 > ord(char) > 96:
+            char = chr(ord(char) - 32)
+        print('{}'.format(char), end="")
+    print('')
--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-    unsigned_num = abs(number)
-    last_digit = unsigned_num % 10
-    print(f"{last_digit}", end="")
-    return(last_digit)
+    l_digit = abs(number) % 10
+    print(f"{l_digit}", end="")
+    return l_digit
