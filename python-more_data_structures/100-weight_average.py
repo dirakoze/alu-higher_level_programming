86ba6cb609bb86ecce9f38afbc678a82093969c1
dirakoze <d.irakoze@alustudent.com>
2023-07-18 01:13:50 -0700

    task

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    else:
-        weight = 0
-        score = 0
-        for i in my_list:
-        weight += (i[0] * i[1])
-        score += (i[1])
-        return weight / score
+    if my_list == []:
+        return (0)
+    number = [item[0] * item[1] for item in my_list]
+    divisor = [item[1] for item in my_list]
+    return (sum(number)/sum(divisor))
